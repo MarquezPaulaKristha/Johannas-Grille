39746071d772cee5161626d77cd8ab65bd53b453
MarquezPaulaKristha <21-02792@g.batstate-u.edu.ph>
2025-02-28 14:40:52 +0800

    save

--- src/pages/Customer/Product/Product.jsx
+++ src/pages/Customer/Product/Product.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import ProductCategory from '../../../components/Customer/Cart/ProductCategory/ProductCategory';
 import ProductDisplay from '../../../components/Customer/Cart/ProductDisplay/ProductDisplay';
+import BranchFilter from '../../../components/Customer/Cart/BranchFilter/BranchFilter';
 
 const Product = ({ orderId }) => { // Proper destructuring of props
   const [category, setCategory] = useState('All');
@@ -8,6 +9,7 @@ const Product = ({ orderId }) => { // Proper destructuring of props
   return (
     <div>
       <ProductCategory category={category} setCategory={setCategory} orderId={orderId} />
+      <BranchFilter />
       <ProductDisplay category={category} orderId={orderId} />
     </div>
   );
