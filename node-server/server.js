633a8cdbce25dbe341d3220b12f10111c16af1a2
MarquezPaulaKristha <21-02792@g.batstate-u.edu.ph>
2025-03-02 19:46:26 +0800

    update

--- node-server/server.js
+++ node-server/server.js
@@ -1403,7 +1403,7 @@ app.post('/api/customer-gcash-checkout', async (req, res) => {
         headers: {
           accept: 'application/json',
           'Content-Type': 'application/json',
-          Authorization: `Basic ${Buffer.from(process.env.PAYMONGO_SECRET_KEY).toString('base64')}`,
+          Authorization: `Basic ${Buffer.from(PAYMONGO_SECRET_KEY).toString('base64')}`,
         },
       }
     );
